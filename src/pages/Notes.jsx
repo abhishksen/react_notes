ab1ace8f073a6b5fb17e453ad75a639dccb913ac
Abhishek Sen <codmonk21@gmail.com>
2023-01-13 12:43:13 +0530

    some changes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -31,7 +31,7 @@ const Notes = ({notes}) => {
         <button className='btn' onClick={() => setShowSearch(prevState => !prevState)}>{showSearch ? <MdClose/> : <CiSearch />}</button>
       </header>
       <div className="notes__container">
-        {filteredNotes.length == 0 && <p className='empty__notes'>Oops! Nothing matched your search</p>}
+        {filteredNotes.length == 0 && <p className='empty__notes'>No data found, start adding your notes</p>}
         {
           filteredNotes.map(note => <NoteItem key = {note.id}
             note = {note} />)
